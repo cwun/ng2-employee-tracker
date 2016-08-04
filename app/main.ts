702ca891ef6f70766c2a94a54accf74083c605b1
Cathy Wun <cathyhwun@gmail.com>
2016-08-04 18:40:10 -0500

    Fixed routing issue on page reload

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,12 @@
 // main entry point
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
 import { AppComponent } from './app.component';
 import { appRouterProviders } from './app.routes';
 
 bootstrap(AppComponent, [
     appRouterProviders
+    ,{provide: LocationStrategy, useClass: HashLocationStrategy}
 ])
 .catch(err => console.error(err));
